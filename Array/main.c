0a4152c3ecb9ca817e9e79439f65f96abf60d8f6
Aagman Suri <aagmansuri96@gmail.com>
2020-11-16 21:34:43 +0530

    Update main.c

--- Array/main.c
+++ Array/main.c
@@ -1,14 +1,29 @@
-//
-//  main.c
-//  Array
-//
-//  Created by Aagman Suri on 16/11/20.
-//
-
 #include <stdio.h>
+#include <stdlib.h>
+//this is the Multi-dimension
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+int main()
+{     int i,j;
+      int *b[3];
+      b[0]= (int *)malloc(4*sizeof(int));
+      b[1]= (int *)malloc(4*sizeof(int));
+      b[2]= (int *)malloc(4*sizeof(int));
+    for(i=0;i<3;i++)
+    {    printf("Enter the elements for %d row",i+1);
+        for(j=0;j<4;j++)
+        {
+            scanf("%d",&b[i][j]);
+        }
+        printf("\n");
+    }
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<4;j++)
+        {
+            printf("%d\t",b[i][j]);
+        }
+        printf("\n");
+    }
+
+
 }
